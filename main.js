4bee478438d9b0e56abc462480a3743fa142211c
NiQuiP <syahrilbenn0731@gmail.com>
2024-08-30 13:12:46 +0700

    new-main

---
+++ main.js
@@ -0,0 +1,86 @@
+const endpoint = "http://192.168.1.10";
+
+function setDapurLed() {
+  fetch(endpoint + "/dapur", {
+    method: "POST",
+  })
+    .then((response) => response)
+    .then(() => location.reload());
+  ledDapur.style.backgroundColor = "red";
+  dapurLedImage.src = "assets/led-on.png";
+}
+function setTamuLed() {
+  fetch(endpoint + "/tamu", {
+    method: "POST",
+  })
+    .then((response) => response)
+    .then(() => location.reload());
+  ledTamu.style.backgroundColor = "red";
+  tamuLedImage.src = "assets/led-on.png";
+}
+function setMakanLed() {
+  fetch(endpoint + "/makan", {
+    method: "POST",
+  })
+    .then((response) => response)
+    .then(() => location.reload());
+  ledMakan.style.backgroundColor = "red";
+  makanLedImage.src = "assets/led-on.png";
+}
+function setToiletLed() {
+  ledToilet.style.backgroundColor = "red";
+  toiletLedImage.src = "assets/led-on.png";
+}
+
+function getDapurLed() {
+  fetch(endpoint + "/dapur", {
+    method: "GET",
+  })
+    .then((response) => response.text())
+    .then((result) => {
+      if (result == "ON") {
+        ledDapur.style.backgroundColor = "red";
+        ledDapur.innerText = "TURN OFF";
+        dapurLedImage.src = "assets/led-on.png";
+      } else {
+        ledDapur.style.backgroundColor = "#579fff";
+        dapurLedImage.src = "assets/led-off.png";
+      }
+    });
+}
+function getTamuLed() {
+  fetch(endpoint + "/tamu", {
+    method: "GET",
+  })
+    .then((response) => response.text())
+    .then((result) => {
+      if (result == "ON") {
+        ledTamu.style.backgroundColor = "red";
+        ledTamu.innerText = "TURN OFF";
+        tamuLedImage.src = "assets/led-on.png";
+      } else {
+        ledTamu.style.backgroundColor = "#579fff";
+        tamuLedImage.src = "assets/led-off.png";
+      }
+    });
+}
+function getMakanLed() {
+  fetch(endpoint + "/makan", {
+    method: "GET",
+  })
+    .then((response) => response.text())
+    .then((result) => {
+      if (result == "ON") {
+        ledMakan.style.backgroundColor = "red";
+        ledMakan.innerText = "TURN OFF";
+        makanLedImage.src = "assets/led-on.png";
+      } else {
+        ledMakan.style.backgroundColor = "#579fff";
+        makanLedImage.src = "assets/led-off.png";
+      }
+    });
+}
+
+getDapurLed();
+getTamuLed();
+getMakanLed();
